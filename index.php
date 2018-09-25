05b89bd4bc99579f487ef9b7248d4a7075eb15c8
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-09-25 16:27:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -218,7 +218,7 @@ if($method == 'POST')
 		$xsjs_url .= "&MTH=$MTH";
 		$xsjs_url .= "&QTR=$QTR";
 		$xsjs_url .= "&NUM=$NUM";
-	echo $xsjs_url;
+	//echo $xsjs_url;
 		if($action == 'HighLowValues')
 		{
 			$username    = "SANYAM_K";

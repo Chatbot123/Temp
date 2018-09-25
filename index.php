db7e6d4942d010d532c50580bdca77ac0e862863
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-09-25 14:49:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -272,6 +272,7 @@ if($method == 'POST')
 			if($YR != '0')	{      $disyear = " for year ";} else {$disyear = "";}
 			if($QTR != '0')	{      $disqtr = " in quarter ";} else {$disqtr = "";}
 			if($MTH != '0')	{      $dismth = " for month ";} else {$dismth = "";}
+			$speech .= $distext;
 			foreach ($someobj["results"] as $value) 
 			{
 				$speech .= $show_dlr. $value["AMOUNT"].$disshop.$value["SHOP_NAME"].$discity.$value["CITY"].$disstate.$value["STATE"]." ".$value["FAMILY_NAME"].$disfamily." ".$value["CATEGORY"].$discategory." ".$value["ARTICLE_LABEL"].$disarticle.$disqtr.$value["QTR"].$dismth.$value["MTH"].$disyear.$value["YR"];

5308ed65649769bd6c227b1ec629032dc82f60f6
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-09-25 12:46:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -199,7 +199,7 @@ if($method == 'POST')
 		if($ENT_TOP_BOT=="" ){$ENT_TOP_BOT='0'; } else {  $xsjs_url .= "&ENT_TOP_BOT=$ENT_TOP_BOT";}
 		if($ENT_MEASURE=="" ){$ENT_MEASURE='0'; } else {  $xsjs_url .= "&ENT_MEASURE=$ENT_MEASURE";}
 		if($NUM = "") {	$NUM='0'; } 
-		$xsjs_url .= "&$NUM=$NUM";
+		$xsjs_url .= "&NUM=$NUM";
 	echo $xsjs_url;
 		if($action == 'HighLowValues')
 		{
